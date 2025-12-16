d42d1bf29979d563950706700045b563034377c3
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-16 17:33:39 +0900

    refactor: Lv3. 할일 전체 조회 시 검색 조건 추가
    - JPQL 사용
    - weather, 기간 선택 조건으로 검색 가능

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user u " +
+            "WHERE (:weather IS NULL OR t.weather = :weather)" +
+            "AND (:start IS NULL OR t.modifiedAt >= :start )" +
+            "AND (:end IS NULL OR t.modifiedAt <= :end)" +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable,
+                                              @Param("weather") String weather,
+                                              @Param("start") LocalDateTime startDate,
+                                              @Param("end") LocalDateTime endDate);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
